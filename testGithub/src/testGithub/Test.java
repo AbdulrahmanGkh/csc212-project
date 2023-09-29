219e28f1478dc0989affcfdb57da3eded125fd49
Ibrahimgh17 <146395629+Ibrahimgh17@users.noreply.github.com>
2023-09-29 13:56:02 +0300

    Update Test.java

--- testGithub/src/testGithub/Test.java
+++ testGithub/src/testGithub/Test.java
@@ -2,8 +2,10 @@ package testGithub;
 
 public class Test {
 
-	public static void main(String[] args) {
-System.out.println("hello world");
+public static void main(String[] args) {
+	System.out.println("hello world");
+	System.out.println("Hello");
+		
 	}
 
 }
