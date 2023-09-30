c239c52646aa7d9e751af46b80bcffff48d057fb
Ibrahimgh17 <146395629+Ibrahimgh17@users.noreply.github.com>
2023-09-30 21:50:10 +0300

    Update Node.java

--- Project/src/Node.java
+++ Project/src/Node.java
@@ -1,33 +1,33 @@
 
 public class Node <T>{
 	
-	public Node<T> next;
-	public T data;
+public Node<T> next;
+public Contact data;
 	
-	public Node() {
-		data=null;
-		next=null;
-	}
+public Node() {
+	data=null;
+	next=null;
+}
 	
-	public Node(T val) {
-		data=val;
-		next=null;
-	}
+public Node(Contact val) {
+	data=val;
+	next=null;
+}
 
-	public Node<T> getNext() {
-		return next;
-	}
+public Node<T> getNext() {
+	return next;
+}
 
-	public void setNext(Node<T> next) {
-		this.next = next;
-	}
+public void setNext(Node<T> next) {
+	this.next = next;
+}
 
-	public T getData() {
-		return data;
-	}
+public Contact getData() {
+	return data;
+}
 
-	public void setData(T data) {
-		this.data = data;
-	}
-	
+public void setData(Contact data) {
+	this.data = data;
+}
+
 }
