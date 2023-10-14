6c08ba157f32e32dbe47b6ee21a5460bbf64960c
Ibrahimgh17 <146395629+Ibrahimgh17@users.noreply.github.com>
2023-10-14 21:57:42 +0300

    Update Node.java
    new

--- Project/src/Node.java
+++ Project/src/Node.java
@@ -1,44 +1,33 @@
 
 public class Node <T>{
 	
-public Node<T> next;
-public Contact data;
-public Node<T> previous;
+	public Node<T> next;
+	public T data;
 	
-public Node() {
-	data=null;
-	next=null;
-	previous = null;
-}
+	public Node() {
+		data=null;
+		next=null;
+	}
 	
-public Node(Contact val) {
-	data=val;
-	next=null;
-	previous = null;
-}
+	public Node(T val) {
+		data=val;
+		next=null;
+	}
 
-public Node<T> getPrevious() {
-	return previous;
-}
+	public Node<T> getNext() {
+		return next;
+	}
 
-public void setPrevious(Node<T> previous) {
-	this.previous = previous;
-}
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
 
-public Node<T> getNext() {
-	return next;
-}
+	public T getData() {
+		return data;
+	}
 
-public void setNext(Node<T> next) {
-	this.next = next;
-}
-
-public Contact getData() {
-	return data;
-}
-
-public void setData(Contact data) {
-	this.data = data;
-}
+	public void setData(T data) {
+		this.data = data;
+	}
 
 }
