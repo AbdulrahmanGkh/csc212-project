56d44c4d1be12384491d0bd554808381219db081
Ibrahimgh17 <146395629+Ibrahimgh17@users.noreply.github.com>
2023-10-15 21:16:29 +0300

    Update LinkedListADT.java
    adding addContact, serach

--- Project/src/LinkedListADT.java
+++ Project/src/LinkedListADT.java
@@ -31,15 +31,15 @@ public class LinkedListADT <T extends Comparable<T>> {
 		current=current.next;
 	}
 	
-	public T retrive() {// Big O(1)
+	public Contact retrive() {// Big O(1)
 		return current.data;
 	}
 	
-	public void ubdate(T val) {// Big O(1)
+	public void ubdate(Contact val) {// Big O(1)
 		current.data=val;
 	}
 
-	public void insertSort(T val) {
+	public void insertSort(Contact val) {
 		if(isEmpty())
 			head=current=new Node<T>(val);
 		else {
@@ -61,7 +61,7 @@ public class LinkedListADT <T extends Comparable<T>> {
 		}
 	}
 	
-	public void insert(T val) { // Big O(1)
+	public void insert(Contact val) { // Big O(1)
 		Node<T> tmp;
 		if(isEmpty()) {
 			current=head=new Node<T>(val);
@@ -74,7 +74,7 @@ public class LinkedListADT <T extends Comparable<T>> {
 			}
 		}
 	
-	public boolean search(T val) {
+	public boolean search(Contact val) {
 		if(head==null)
 			return false;
 		Node<T> temp= head;
@@ -88,7 +88,7 @@ public class LinkedListADT <T extends Comparable<T>> {
 		return false;
 	}
 	
-   public void delete(T val) {
+   public void delete(Contact val) {
 	   if (current == head)
 		   head = head.next;
 	   else {
@@ -101,119 +101,119 @@ public class LinkedListADT <T extends Comparable<T>> {
 			   current = head;
 		   else
 			   current = current.next;
-	 }	   
-   }
-   /*
-	public void add(String name,String phoneNumber,String emailAddres,String address,String birthday,String notes)throws IllegalArgumentException  { // big O(1)
-		if(searchName(name)!=null || searchPhoneNB(phoneNumber) != null)
-			throw new IllegalArgumentException("The contact is already added");
-		else {
+	 }
+
+public void addContact(String name,String phoneNumber,String emailAddres,String address,String birthday,String notes) { // big O(n)
 		Contact c = new Contact(name,phoneNumber,emailAddres,address,birthday,notes);
-		Node<T> temp= new Node<>(c);
 		if(isEmpty())
 			current = head = new Node<>(c);
-		else
-		{
-		temp.next=current.next;
-		temp.previous=current;
-		if(current.next !=null)
-		current.next.previous = temp;
-		current.next=temp;
-		current = temp;
-		}
-		}
-	} */
-   
-   
-   
-   /*
-	public Contact searchName(String name) {// Big O(n)
-		if(head==null)
-			return true;// there is no Contacts
-		Node<T> temp = head;
-		while(temp!=null) {
-			if(temp.getData().getName().equalsIgnoreCase(name))
-				return true;// return the contact with the same name
-			temp=temp.getNext();
+		else {
+			current=head;
+			if(c.compareTo(head.getData())<0) {// The name is first alphabetically
+				Node<T> temp = head;
+				head = new Node<T>(c);
+				head.next = temp;
 			}
-		return false;
-		
+			else {
+				while(current.next!=null && c.compareTo(current.next.data) > 0) {
+					current=current.next; ; // the name goes to its alphabetical place
+					Node<T> temp = current.next;
+					current.next = new Node<T>(c);
+					current = current.next;
+					current.next = temp;
+				}	
+			}
+		}
 	}
-	public void searchEmail(String emailAddress) {// Big O(n)
-		if(head==null)
-			System.out.println("There is no contacts");// there is no Contacts
-		Node<T> temp = head;
-		boolean exist = false;// this is our indicator if there is any contacts with same email address or not
-		while(temp!=null) {
-			if(temp.getData().getEmailAddress().equals(emailAddress)) {// to print every contact with the same email address
-				System.out.println("Contact found!");
-				System.out.println("Name:"+ temp.getData().getName());
-				System.out.println("Phone Number: "+ temp.getData().getPhoneNumber());
-				System.out.println("Email Address: "+ temp.getData().getEmailAddress());
-				System.out.println("Birthday:"+ temp.getData().getBirthday());
-				System.out.println("Notes: "+ temp.getData().getNotes());
-				exist=true;// we found contacts
-			}
-			temp=temp.getNext();	
-		}
-		if(!exist)
-			System.out.println("There is no contact with the same email address ");
 		
-	} 
-	public Contact searchPhoneNB(String phoneNumber) { // Big O(n)
-		if(head==null)
-			return null; // there is no Contacts
-		Node<T> temp = head;
-		while(temp!=null) {
-			if(temp.getData().getPhoneNumber().equals(phoneNumber))
-				return temp.getData();// return the contact with the same phone number
-			temp=temp.getNext();
-		}
-		return null;// there is no contract with the same phone number
-	} 
 	
-	public void searchAddress(String address) {// Big O(n)
+	 public Contact searchName(String name) {// Big O(n)
 		if(head==null)
-			System.out.println("There is no contacts");// there is no Contacts
-		Node<T> temp = head;
-		boolean exist = false;// this is our indicator if there is any contacts with same  address or not
-		while(temp!=null) {
-			if(temp.getData().getEmailAddress().equals(address)) {// to print every contact with the same  address
-				System.out.println("Contact found!");
-				System.out.println("Name:"+ temp.getData().getName());
-				System.out.println("Phone Number: "+ temp.getData().getPhoneNumber());
-				System.out.println("Email Address: "+ temp.getData().getEmailAddress());
-				System.out.println("Birthday:"+ temp.getData().getBirthday());
-				System.out.println("Notes: "+ temp.getData().getNotes());
-				exist=true;// we found contacts
-			}
-			temp=temp.getNext();	
+				return null;// there is no Contacts
+			Node<T> temp = head;
+			while(temp!=null) {
+				if(temp.getData().getName().equalsIgnoreCase(name))
+					return temp.getData();// return the contact with the same name
+				temp=temp.getNext();
+				}
+			return null;
+			
 		}
-		if(!exist)
-			System.out.println("There is no contact with the same address ");
+	
+	 public Contact searchPhoneNB(String phoneNumber) { // Big O(n)
+	 	 if(head==null)
+	 			return null; // there is no Contacts
+	 		Node<T> temp = head;
+	 		while(temp!=null) {
+	 			if(temp.getData().getPhoneNumber().equals(phoneNumber))
+						return temp.getData();// return the contact with the same phone number
+	 			temp=temp.getNext();
+	 			}
+	 			return null;// there is no contract with the same phone number
+	 		}  
+	 
+	 public void searchEmail(String emailAddress) {// Big O(n)
+			if(head==null)
+				System.out.println("There is no contacts");// there is no Contacts
+			Node<T> temp = head;
+			boolean exist = false;// this is our indicator if there is any contacts with same email address or not
+			while(temp!=null) {
+				if(temp.getData().getEmailAddress().equals(emailAddress)) {// to print every contact with the same email address
+					System.out.println("Contact found!");
+					System.out.println("Name:"+ temp.getData().getName());
+					System.out.println("Phone Number: "+ temp.getData().getPhoneNumber());
+					System.out.println("Email Address: "+ temp.getData().getEmailAddress());
+					System.out.println("Birthday:"+ temp.getData().getBirthday());
+					System.out.println("Notes: "+ temp.getData().getNotes());
+					exist=true;// we found contacts
+				}
+				temp=temp.getNext();	
+			}
+			if(!exist)
+				System.out.println("There is no contact with the same email address ");
+		} 
+	 
+	 public void searchAddress(String address) {// Big O(n)
+			if(head==null)
+				System.out.println("There is no contacts");// there is no Contacts
+			Node<T> temp = head;
+			boolean exist = false;// this is our indicator if there is any contacts with same  address or not
+			while(temp!=null) {
+				if(temp.getData().getEmailAddress().equals(address)) {// to print every contact with the same  address
+					System.out.println("Contact found!");
+					System.out.println("Name:"+ temp.getData().getName());
+					System.out.println("Phone Number: "+ temp.getData().getPhoneNumber());
+					System.out.println("Email Address: "+ temp.getData().getEmailAddress());
+					System.out.println("Birthday:"+ temp.getData().getBirthday());
+					System.out.println("Notes: "+ temp.getData().getNotes());
+					exist=true;// we found contacts
+				}
+				temp=temp.getNext();	
+			}
+			if(!exist)
+				System.out.println("There is no contact with the same address ");
+		}
+	 
+	 public void searchBirthday(String birthday) {// Big O(n)
+			if(head==null)
+				System.out.println("There is no contacts");// there is no Contacts
+			Node<T> temp = head;
+			boolean exist = false;// this is our indicator if there is any contacts with same  birth date or not
+			while(temp!=null) {
+				if(temp.getData().getEmailAddress().equals(birthday)) {// to print every contact with the same  birth date
+					System.out.println("Contact found!");
+					System.out.println("Name:"+ temp.getData().getName());
+					System.out.println("Phone Number: "+ temp.getData().getPhoneNumber());
+					System.out.println("Email Address: "+ temp.getData().getEmailAddress());
+					System.out.println("Birthday:"+ temp.getData().getBirthday());
+					System.out.println("Notes: "+ temp.getData().getNotes());
+					exist=true;// we found contacts
+				}
+				temp=temp.getNext();	
+			}
+			if(!exist)
+				System.out.println("There is no contact with the same birth date ");
 		
-	}
 	
-	public void searchBirthday(String birthday) {// Big O(n)
-		if(head==null)
-			System.out.println("There is no contacts");// there is no Contacts
-		Node<T> temp = head;
-		boolean exist = false;// this is our indicator if there is any contacts with same  birth date or not
-		while(temp!=null) {
-			if(temp.getData().getEmailAddress().equals(birthday)) {// to print every contact with the same  birth date
-				System.out.println("Contact found!");
-				System.out.println("Name:"+ temp.getData().getName());
-				System.out.println("Phone Number: "+ temp.getData().getPhoneNumber());
-				System.out.println("Email Address: "+ temp.getData().getEmailAddress());
-				System.out.println("Birthday:"+ temp.getData().getBirthday());
-				System.out.println("Notes: "+ temp.getData().getNotes());
-				exist=true;// we found contacts
-			}
-			temp=temp.getNext();	
-		}
-		if(!exist)
-			System.out.println("There is no contact with the same birth date ");
-	} 
-	*/
-	
-
+   }
+  
